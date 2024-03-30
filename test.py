7490d219f7db397487a9413c0fad8de4bcb203c4
samy <samycherief21@outlook.fr>
2024-03-30 01:04:38 +0100

    instanciation des taches

--- test.py
+++ test.py
@@ -23,23 +23,10 @@ def runTsomme():
     print("execution de tsomme : ", Z)
 
 
-t1 = Task()
-t1.name = "T1"
-t1.writes = ["X"]
-t1.run = runT1
-
-
-t2 = Task()
-t2.name = "T2"
-t2.writes = ["T2"]
-t2.run = runT2
-
-tsomme = Task()
-tsomme.name = "somme"
-tsomme.reads = ["X", "Y"]
-tsomme.writes = ["Z"]
-tsomme.run = runTsomme
+t1 = Task("T1", [], ["X"], runT1)
+t2 = Task("T2", ["X"], ["Y"], runT2)
+tSomme = Task("somme", ["X", "Y"], ["Z"], runTsomme)
 
 t1.run()
 t2.run()
-tsomme.run()
+tSomme.run()
