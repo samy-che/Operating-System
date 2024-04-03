ddaed18026343cad775cd69c3477dc90014fec27
samy <samycherief21@outlook.fr>
2024-04-03 17:28:22 +0200

    ajout du run sequentielle et de l'affichage

--- maxpar.py
+++ maxpar.py
@@ -1,4 +1,6 @@
 import threading  # importer la Librairie pour la gestion des Threads
+import networkx
+import matplotlib.pyplot as plt
 
 
 # classe de Tâche
@@ -12,27 +14,11 @@ class Task:
 
 
 class TaskSystem:
-    def __init__(self, task, parent):
+    def __init__(self, task, dico):
         self.task = task
-        self.parent = parent
+        self.dico = dico
         self.valid()
-
-    # renvoie la liste de dependence pour une tache selon le système de parallélisme maximal
-    def getDependencies(self, nomTache):
-
-        # si la tache n'a aucune dependance cas exeptionel
-        if self.parent["nomTache"] == []:
-            return self.parent["nomTache"]
-        else:
-            # pour chaque dependance de notre tache :
-            for element in self.parent["nomTache"]:
-                # pour la dependence on verifie si parmis ces dependance y'en a une qui appartient a la liste de dependence de nomTache
-                for dependance in self.parent["element"]:
-                    if dependance in self.parent["nomTache"]:
-                        # supprimer la dependance qui sert a rien de la liste
-                        self.parent["nomTache"].remove(dependance)
-
-        return self.parent["nomTache"]
+        self.graph = self.draw()
 
     def valid(self):
         # vérifie que les noms des tâches sont uniques
@@ -48,32 +34,79 @@ class TaskSystem:
 
             # vérifie que toutes les tâches du système de tâches font partie du dictionnaire.
             temp = False
-            for key in self.parent.keys():
+            for key in self.dico.keys():
                 if task_names[i] == key:
                     temp = True
             if temp == False:
                 raise ValueError(
                     "La tâche à l'indice {} dans le système des tâches ne fait pas partie du dictionnaire.".format(i))
 
-        for key in self.parent.keys():
+        for key in self.dico.keys():
             # vérifie la cohérence entre les noms des tâches dans la liste des tâches et ceux dans le graphe de précédence
             if key not in task_names:
                 raise ValueError(
                     f"Le nom de tâche {key} dans le dictionnaire de précédence n'est pas dans la liste des tâches")
 
              # vérifie que la clé "tâche" ne doit pas être précéder par elle-même.
-            if key in self.parent[key]:
+            if key in self.dico[key]:
                 raise ValueError(
                     "Erreur : La clé est présente dans sa propre valeur.")
 
             # vérifie si des tâches ne sont pas mutuellement dépandante
-            for key2 in self.parent.keys():
-                if key in self.parent[key2] and key2 in self.parent[key]:
+            for key2 in self.dico.keys():
+                if key in self.dico[key2] and key2 in self.dico[key]:
                     raise ValueError("les tâches sont mutuellement dependante")
 
         # verifie si la dépendance est dans la liste des tâches
-        for dependencies in self.parent.values():
+        for dependencies in self.dico.values():
             for dep in dependencies:
                 if dep not in task_names:
                     raise ValueError(
                         f"La dépendance {dep} n'est pas dans la liste des tâches")
+
+    # renvoie la liste de dependence pour une tache selon le système de parallélisme maximal,
+    def getDependencies(self, task):
+
+        # si la tache n'a aucune dependance cas exeptionel
+        if self.dico[task.name] == []:
+            return self.dico[task.name]
+        else:
+            # pour chaque dependance de notre tache :
+            for element in self.dico[task.name]:
+                # pour la dependence on verifie si parmis ces dependance y'en a une qui appartient a la liste de dependence de nomTache
+                for dependance in self.dico[element]:
+                    if dependance in self.dico[task.name]:
+                        # supprimer la dependance qui sert a rien de la liste
+                        self.dico[task.name].remove(dependance)
+
+        return self.dico[task.name]
+
+    # exécute les tâches de façon séquentielle en respectant l’ordre imposé par la relation de précédence
+    def runSeq(self):
+        # effectue un tri topologique sur le graphe pour obtenir l'ordre d'exécution des tâches
+        task_ord = list(networkx.topological_sort(self.graph))
+        # parcour les tâches dans l'ordre obtenu
+        for task_name in task_ord:
+            # trouve l'instance de la tâche correspondante à partir de son nom et l'exécute
+            task = next(t for t in self.task if t.name == task_name)
+            task.run()
+
+     # affiche graphiquement le grahe de précédence en utilisant la bilbiothèque "networkx"
+    def draw(self):
+        # crée un graphe dirigé vide
+        graph = networkx.DiGraph()
+
+        for task in self.task:
+            # ajoute un noeud au graphe pour chaque tâche en utilisant son nom
+            graph.add_node(task.name)
+            # parcourir les dépendances de la tâche actuelle
+            for dep in self.getDependencies(task):
+                # ajoute un arc dirigé entre chaque dépendance et la tâche actuelle
+                graph.add_edge(dep, task.name)
+
+        # affiche le graphe dans une fenêtre
+        pos = networkx.spring_layout(graph)
+        networkx.draw(graph, pos, with_labels=True, node_size=2000,
+                      node_color="lightblue", font_size=10, font_weight="bold")
+        plt.show()
+        return graph
